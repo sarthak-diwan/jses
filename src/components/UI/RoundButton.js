67a13e27243aa0ac8ae68fe7aaaea456df64691e
Sanath91009 <sanathprime@gmail.com>
2021-12-23 20:46:41 +0530

    updated

--- src/components/UI/RoundButton.js
+++ src/components/UI/RoundButton.js
@@ -1,10 +1,13 @@
-import classes from './RoundButton.module.css'
+import classes from "./RoundButton.module.css";
 const Button = (props) => {
     return (
-        <button className={classes.button} type={props.type || 'button'}
-        onClick={props.onClick}>
-        {props.children}
+        <button
+            className={classes.button}
+            type={props.type || "button"}
+            onClick={props.onClick}
+        >
+            {props.children}
         </button>
     );
-}
+};
 export default Button;
